f41742415d8a84e54ac84ef2c5ac075745bef8ef
stwardak <148924249+stwardak@users.noreply.github.com>
2024-02-05 14:40:02 -0600

    add authentication

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { Logout } from "./Logout";
+
 export function Content() {
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <Signup/>
+      <Login/>
+      <Logout/>
     </main>
   )
 }
